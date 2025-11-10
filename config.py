d799137b8882d9bc28fcc1f96fe8bbd3d8ae42fd
xlito08 <dc.xlito@gmail.com>
2025-11-10 23:16:22 +0400

    Add configuration settings for Discord bot

---
+++ config.py
@@ -0,0 +1,38 @@
+TOKEN = "YOUR_DISCORD_TOKEN"
+
+
+
+
+RULE_CHANNEL_ID = 1344685721960841226 
+
+EMBED_IMG_URL = "https://cdn.discordapp.com/attachments/1261796252463534130/1262762427611877527/banner_1.png?ex=6697c6a7&is=66967527&hm=85074cee521390841a54481147b384fcfc598903d68ed30a65def5aa154f8a7d&" # embed image url
+
+
+
+
+
+
+
+
+SlOT_CATEGORYS = [1344359483635142786,1344359512450273311,1344359531257266207] # slot category_1,slot category_2,slot category_3
+
+SlOT_CATEGORY_1 = 1344359483635142786 # slot category_1 id
+
+SlOT_CATEGORY_2 = 1344359512450273311 # slot category_2 id
+
+SlOT_CATEGORY_3 = 1344359531257266207 # slot category_3 id
+
+
+
+
+
+SERVER_ID = 1344685383501221908 # slot server id . Your slot server id 
+
+SELLER_CHANNEL = 1336772337868996618 # seller channel id where will be ping reset message will be sent
+
+ROLE_ID = 1344668864834703391 # seller role id the role will be given to the slot create 
+ 
+
+
+
+TIMEZONE = "Asia/Dhaka" # timezone
